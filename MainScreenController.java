96e92c47d6687f49f503a10c0eca41b4b67b4e76
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2021-04-28 16:40:19 -0500

    Update MainScreenController.java

--- MainScreenController.java
+++ MainScreenController.java
@@ -1,4 +1,15 @@
-package sample;
-
 public class MainScreenController {
+    @FXML
+    private Button MenuButton;
+
+    public void changeSceneButtonHOme(ActionEvent event) throws IOException {
+        Parent MenuScreen = FXMLLoader.load(getClass().getResource("ChooseClass.fxml"));
+        Scene Homescene = new Scene(MenuScreen);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(Homescene);
+        window.show();
+    }
+
 }
