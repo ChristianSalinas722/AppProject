0fe062c33e723e018deb92356ef60c075f67249f
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2021-04-28 16:51:49 -0500

    Update MainScreenController.java

--- MainScreenController.java
+++ MainScreenController.java
@@ -1,3 +1,18 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+
 public class MainScreenController {
     @FXML
     private Button MenuButton;
