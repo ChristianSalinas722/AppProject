720842ede4f8c2c356d8c743ad62aabe897bf68f
Christian Salinas <50554683+ChristianSalinas722@users.noreply.github.com>
2021-04-28 16:52:50 -0500

    Update ViewGradeController.java

--- ViewGradeController.java
+++ ViewGradeController.java
@@ -1,4 +1,4 @@
-package application;
+package sample;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -20,7 +20,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
-public class ViewGradesController {
+public class ViewGradeController {
 
     @FXML
     private Text average;
@@ -33,178 +33,178 @@ public class ViewGradesController {
 
     @FXML
     private TextField grade;
-    
+
     @FXML
     private TextFlow gradeList;
-    
+
     @FXML
     private TextFlow assignList;
 
     @FXML
     void addGrade(ActionEvent event) throws IOException {
-    	String ass = assignment.getText().toString();
-    	String gra = grade.getText().toString();
-    	
-    	grade.clear();
-    	assignment.clear();
-    	
-    	updateGradeList(ass, gra);
-    	
-    	double result = calculateAverage();
-    	average.setText(Double.toString(result));
+        String ass = assignment.getText().toString();
+        String gra = grade.getText().toString();
+
+        grade.clear();
+        assignment.clear();
+
+        updateGradeList(ass, gra);
+
+        double result = calculateAverage();
+        average.setText(Double.toString(result));
     }
-    
+
     //function to calculate average, was thinking we can just pass in path to text file, load text file in function, parse grades and calculate average, then return average
-    
+
     public double calculateAverage() throws NumberFormatException, IOException {
-    	double average = 0;
-    	double total = 0;
-    	
-    	File fileToBeModified = new File("Physics.txt");
-    	BufferedReader br = null;
-    	br = new BufferedReader(new FileReader(fileToBeModified));
-    	if(fileToBeModified.length() <= 1)
-    	{
-    		System.out.println("File is empty");
-    		return 0;
-    	}
-    	FileWriter fw = null;
-    	String line = "";
-    	String currentLine = "";
-    	String assign = "";
-    	String grad = "";
-    	int lineNum = 0;
-    	int result = 0;
-    	
-    	String patern = "(.*)([1-9]{2}$)";
-    	Pattern r = Pattern.compile(patern);
-    	Matcher m = r.matcher(currentLine);
+        double average = 0;
+        double total = 0;
 
-    	if(m.find()){
-    	grad = m.group(2);
-    	} 
-    	
-            while (( line = br.readLine()) != null) {											//read to end of file
-        
-            	currentLine = line + "\n";
-            	patern = "(.*)([1-9]{2}$)";
-            	r = Pattern.compile(patern);
-            	m = r.matcher(currentLine);
-
-            	if(m.find()){
-            	assign = m.group(1);
-            	grad = m.group(2);
-            	} 
-            	total += Double.parseDouble(grad);
-            	System.out.println(assign);
-            	System.out.println(grad);
-            	
-            	
-            	lineNum++;
-            	if(total != 0) {
-            		result = 1;
-            	}
-            }
-            
-            System.out.println("Line Number total: " + lineNum);
-            if(result == 1) {
-            average = total / lineNum;
-            
-            
-    	
-    	return average;
-            }
+        File fileToBeModified = new File("Physics.txt");
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(fileToBeModified));
+        if(fileToBeModified.length() <= 1)
+        {
+            System.out.println("File is empty");
             return 0;
+        }
+        FileWriter fw = null;
+        String line = "";
+        String currentLine = "";
+        String assign = "";
+        String grad = "";
+        int lineNum = 0;
+        int result = 0;
+
+        String patern = "(.*)([1-9]{2}$)";
+        Pattern r = Pattern.compile(patern);
+        Matcher m = r.matcher(currentLine);
+
+        if(m.find()){
+            grad = m.group(2);
+        }
+
+        while (( line = br.readLine()) != null) {											//read to end of file
+
+            currentLine = line + "\n";
+            patern = "(.*)([1-9]{2}$)";
+            r = Pattern.compile(patern);
+            m = r.matcher(currentLine);
+
+            if(m.find()){
+                assign = m.group(1);
+                grad = m.group(2);
+            }
+            total += Double.parseDouble(grad);
+            System.out.println(assign);
+            System.out.println(grad);
+
+
+            lineNum++;
+            if(total != 0) {
+                result = 1;
+            }
+        }
+
+        System.out.println("Line Number total: " + lineNum);
+        if(result == 1) {
+            average = total / lineNum;
+
+
+
+            return average;
+        }
+        return 0;
     }
-    
-    //We will need to make this method be called when this FXML loads and then call it again every time addGrade button is pressed. Possible parameter is a text file and then it parses/loops 
+
+    //We will need to make this method be called when this FXML loads and then call it again every time addGrade button is pressed. Possible parameter is a text file and then it parses/loops
     //through the txt file and then outputs them in particular format to gradesList
     public void updateGradeList(String assignment, String grade /*, file argument or something */) throws IOException {
-    	
-    	
-		File f = new File("Physics.txt");												//open file
-		PrintWriter pw = new PrintWriter(new FileWriter(f,true)); 					//Create PrintWriter to write to file with filewriter attached to users.txt
-		FileReader fr = new FileReader(f);											//create filereader and buffered reader to parse file
-		BufferedReader br = new BufferedReader(new FileReader(f));
-		String formatStr = "%-20s %-12s";
-	
-		//if(assignment.length() < 5) {
-	
-			pw.write(assignment + " " + grade + "\n");
-			pw.flush();
+
+
+        File f = new File("Physics.txt");												//open file
+        PrintWriter pw = new PrintWriter(new FileWriter(f,true)); 					//Create PrintWriter to write to file with filewriter attached to users.txt
+        FileReader fr = new FileReader(f);											//create filereader and buffered reader to parse file
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String formatStr = "%-20s %-12s";
+
+        //if(assignment.length() < 5) {
+
+        pw.write(assignment + " " + grade + "\n");
+        pw.flush();
 		/*}
 		else if(assignment.length() >= 5 && assignment.length() < 12) {
 			pw.write("\n");
 			pw.write(String.format(formatStr, assignment, grade));
 			pw.flush();
-		
+
 		}
 		else if(assignment.length() >= 12 && assignment.length() < 20) {
 			pw.write("\n");
 			pw.write(String.format(formatStr, assignment, grade));
 			pw.flush();
-		
+
 		}
 		else if(assignment.length() >= 20 && assignment.length() < 28) {
 			pw.append("\n" + assignment + "\t" + grade);
 			pw.flush();
-		
+
 		}*/
-    	gradeList.getChildren().clear();
-    	assignList.getChildren().clear();
-    	pw.close();
-    	initGradeList();
-    	
-    	
- 
+        gradeList.getChildren().clear();
+        assignList.getChildren().clear();
+        pw.close();
+        initGradeList();
+
+
+
     }
-    
-    
-    //read through the file and put all current grades to gradesList with format "assignment \t grade\n" 
+
+
+    //read through the file and put all current grades to gradesList with format "assignment \t grade\n"
     public void initGradeList(/*, file argument or something */) throws IOException {
-    	File fileToBeModified = new File("Physics.txt");
-    	BufferedReader br = null;
-    	br = new BufferedReader(new FileReader(fileToBeModified));
-    	FileWriter fw = null;
-    	String line = "";
-    	String currentLine = "";
-    	String assign = "";
-    	String grad = "";
-    	
-    	
-    	String patern = "(.*)([1-9]{2}$)";
-    	Pattern r = Pattern.compile(patern);
-    	Matcher m = r.matcher(currentLine);
+        File fileToBeModified = new File("Physics.txt");
+        BufferedReader br = null;
+        br = new BufferedReader(new FileReader(fileToBeModified));
+        FileWriter fw = null;
+        String line = "";
+        String currentLine = "";
+        String assign = "";
+        String grad = "";
 
-    	if(m.find()){
-    	assign = m.group(1);
-    	grad = m.group(2);
-    	} 
-    	
-    	String formatStr = "%-20s %-15s";
-            while (( line = br.readLine()) != null) {											//read to end of file
 
-            	currentLine = line + "\n";
-            	patern = "(.*)([1-9]{2}$)";
-            	r = Pattern.compile(patern);
-            	m = r.matcher(currentLine);
+        String patern = "(.*)([1-9]{2}$)";
+        Pattern r = Pattern.compile(patern);
+        Matcher m = r.matcher(currentLine);
 
-            	if(m.find()){
-            	assign = m.group(1);
-            	grad = m.group(2);
-            	} 
-            	
-            	System.out.println(assign);
-            	System.out.println(grad);
-            	
+        if(m.find()){
+            assign = m.group(1);
+            grad = m.group(2);
+        }
+
+        String formatStr = "%-20s %-15s";
+        while (( line = br.readLine()) != null) {											//read to end of file
+
+            currentLine = line + "\n";
+            patern = "(.*)([1-9]{2}$)";
+            r = Pattern.compile(patern);
+            m = r.matcher(currentLine);
+
+            if(m.find()){
+                assign = m.group(1);
+                grad = m.group(2);
+            }
+
+            System.out.println(assign);
+            System.out.println(grad);
+
             //	System.out.println(currentLine);
-            	Text text_1 = new Text(assign + "\n");
-            	assignList.getChildren().add(text_1);
-            	Text text_2 = new Text(grad + "\n");
-            	gradeList.getChildren().add(text_2);
-            	
-            }
-            }
+            Text text_1 = new Text(assign + "\n");
+            assignList.getChildren().add(text_1);
+            Text text_2 = new Text(grad + "\n");
+            gradeList.getChildren().add(text_2);
+
+        }
+    }
 
 
 }
