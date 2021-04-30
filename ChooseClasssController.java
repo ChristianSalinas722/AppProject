3886ebfda03675c86d27379ecb2cab690604657b
IceTitanic <63264472+IceTitanic@users.noreply.github.com>
2021-04-29 20:53:40 -0500

    Update ChooseClasssController.java

--- ChooseClasssController.java
+++ ChooseClasssController.java
@@ -1,4 +1,4 @@
-package application;
+package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -8,7 +8,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,7 +15,11 @@ import java.io.*;
 
 public class ChooseClassController {
 
+    @FXML
     private Button Add_Grade;
+
+    @FXML
+    private Button Delete_Grade;
 
     @FXML
     private Button Select_Class;
@@ -27,23 +30,58 @@ public class ChooseClassController {
     @FXML
     private ListView<String> Class_List;
 
+    private int selectedIndex = -1;
 
-    public void AddClassToList(ActionEvent event) {
+public void DeleteClassToList() {
+
+    String create = Input_Field.getText() + ".txt";
+    String Class = Input_Field.getText();
+
+    File f1 = new File(create);
+
+    boolean success = f1.delete();
+
+    Class_List.setOnMouseClicked(event -> {
+        String selectedItem = Class_List.getSelectionModel().getSelectedItem().toString();
+        Input_Field.setText(selectedItem);
+    });
+
+    Class_List.getItems().remove(Class);
+
+    if (!success) {
+        System.out.println("Deletion Failed.");
+    } else {
+        System.out.println("File deleted.");
+
+    }
+
+    Input_Field.clear();
+}
+
+    public String AddClassToList() {
+
         String create = Input_Field.getText() + ".txt";
         String Class = Input_Field.getText();
 
+        Class_List.setOnMouseClicked(event -> {
+            String selectedItem = Class_List.getSelectionModel().getSelectedItem().toString();
+            selectedIndex = Class_List.getSelectionModel().getSelectedIndex();
+            Input_Field.setText(selectedItem);
+        });
 
             try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(create,true)))){
-                out.println(Input_Field.getText());
+                out.println();
             } catch(IOException ioe){
                 ioe.printStackTrace();
             }
 
         Class_List.getItems().addAll(Class);
-        Input_Field.clear();
-      Class_List.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-       }
 
+
+        Input_Field.clear();
+
+      return create;
+       }
 
        public void changeSceneButtonHOme(ActionEvent event) throws IOException {
         Parent MenuScreen = FXMLLoader.load(getClass().getResource("Menu.fxml"));
@@ -55,7 +93,6 @@ public class ChooseClassController {
         window.show();
        }
 
+}
 
 
-
-    }
